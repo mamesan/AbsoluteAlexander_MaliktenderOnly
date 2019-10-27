7fc75c617cca9e6deac8ce72aa59254fd0fc553f
mame <hanaochan@live.jp>
2019-10-27 21:52:09 +0900

    コミット

--- AbsoluteAlexander_MaliktenderOnly/AbsoluteAlexander_MaliktenderOnly/Alexander.Designer.cs
+++ AbsoluteAlexander_MaliktenderOnly/AbsoluteAlexander_MaliktenderOnly/Alexander.Designer.cs
@@ -28,18 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.管理 = new System.Windows.Forms.TabPage();
+            this.textBox_list = new System.Windows.Forms.TextBox();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.checkedListBox_init = new System.Windows.Forms.CheckedListBox();
+            this.textBoxlocalPath_init = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.管理.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.管理);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(962, 671);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(954, 645);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // 管理
+            // 
+            this.管理.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.管理.Controls.Add(this.textBox_list);
+            this.管理.Controls.Add(this.button_delete);
+            this.管理.Controls.Add(this.button_add);
+            this.管理.Controls.Add(this.checkedListBox_init);
+            this.管理.Controls.Add(this.textBoxlocalPath_init);
+            this.管理.Controls.Add(this.label2);
+            this.管理.Location = new System.Drawing.Point(4, 22);
+            this.管理.Name = "管理";
+            this.管理.Padding = new System.Windows.Forms.Padding(3);
+            this.管理.Size = new System.Drawing.Size(954, 645);
+            this.管理.TabIndex = 1;
+            this.管理.Text = "管理";
+            // 
+            // textBox_list
+            // 
+            this.textBox_list.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox_list.Location = new System.Drawing.Point(33, 90);
+            this.textBox_list.Name = "textBox_list";
+            this.textBox_list.Size = new System.Drawing.Size(227, 28);
+            this.textBox_list.TabIndex = 13;
+            // 
+            // button_delete
+            // 
+            this.button_delete.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button_delete.Location = new System.Drawing.Point(357, 90);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(75, 28);
+            this.button_delete.TabIndex = 15;
+            this.button_delete.Text = "Delete";
+            this.button_delete.UseVisualStyleBackColor = true;
+            // 
+            // button_add
+            // 
+            this.button_add.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button_add.Location = new System.Drawing.Point(276, 90);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(75, 28);
+            this.button_add.TabIndex = 14;
+            this.button_add.Text = "add";
+            this.button_add.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox_init
+            // 
+            this.checkedListBox_init.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkedListBox_init.FormattingEnabled = true;
+            this.checkedListBox_init.Location = new System.Drawing.Point(33, 124);
+            this.checkedListBox_init.Name = "checkedListBox_init";
+            this.checkedListBox_init.Size = new System.Drawing.Size(399, 256);
+            this.checkedListBox_init.TabIndex = 16;
+            // 
+            // textBoxlocalPath_init
+            // 
+            this.textBoxlocalPath_init.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxlocalPath_init.Location = new System.Drawing.Point(87, 39);
+            this.textBoxlocalPath_init.Name = "textBoxlocalPath_init";
+            this.textBoxlocalPath_init.Size = new System.Drawing.Size(345, 28);
+            this.textBoxlocalPath_init.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(30, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Path:";
             // 
             // Alexander
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tabControl1);
             this.Name = "Alexander";
-            this.Size = new System.Drawing.Size(781, 445);
+            this.Size = new System.Drawing.Size(962, 671);
+            this.tabControl1.ResumeLayout(false);
+            this.管理.ResumeLayout(false);
+            this.管理.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage 管理;
+        private System.Windows.Forms.TextBox textBox_list;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.CheckedListBox checkedListBox_init;
+        private System.Windows.Forms.TextBox textBoxlocalPath_init;
+        private System.Windows.Forms.Label label2;
     }
 }
--- AbsoluteAlexander_MaliktenderOnly/AbsoluteAlexander_MaliktenderOnly/Alexander.cs
+++ AbsoluteAlexander_MaliktenderOnly/AbsoluteAlexander_MaliktenderOnly/Alexander.cs
@@ -8,16 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Advanced_Combat_Tracker;
+using System.IO;
 
 namespace AbsoluteAlexander_MaliktenderOnly
 {
     public partial class Alexander : UserControl, IActPluginV1
     {
         private SettingsSerializer xmlSettings;
+        // チェックの入った物をすべて格納しておく
+        private List<string> scanList = new List<string>();
+        private string fileFullPath_main_local_scan;
+        // 選択アイテム名を格納する
+        private string select_item_name = "";
 
         public Alexander()
         {
             InitializeComponent();
+            ActHelper.Initialize();
         }
 
         public void DeInitPlugin()
@@ -33,8 +40,8 @@ namespace AbsoluteAlexander_MaliktenderOnly
             this.Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
                                         // MultiProject.BasePlugin.xmlSettings = new SettingsSerializer(this); // Create a new settings serializer and pass it this instance
 
-            pluginScreenSpace.Text = "Eden";
-            pluginStatusText.Text = "EdenDesignatedPluginStarts";
+            pluginScreenSpace.Text = "アレキ";
+            pluginStatusText.Text = "AbsoluteAlexanderPluginStarts";
 
             // インターフェイス情報を格納する
             this.xmlSettings = new SettingsSerializer(this);
@@ -58,9 +65,16 @@ namespace AbsoluteAlexander_MaliktenderOnly
             // ログを取得するイベントを生成する
             ActGlobals.oFormActMain.OnLogLineRead += OFormActMain_OnLogLineRead;
 
+            scanList = new List<string>();
+            // scanlistに要素を格納する
+            if (checkedListBox_init.CheckedItems.Count != 0)
+            {
+                for (int x = 0; x < checkedListBox_init.CheckedItems.Count; x++)
+                {
+                    scanList.Add(checkedListBox_init.CheckedItems[x].ToString());
+                }
+            }
 
-
-            throw new NotImplementedException();
         }
 
         private void OFormActMain_OnLogLineRead(bool isImport, LogLineEventArgs logInfo)
@@ -75,6 +89,216 @@ namespace AbsoluteAlexander_MaliktenderOnly
             // 戦闘開始のお知らせ
             if (logInfo.logLine.Contains("戦闘開始！"))
             {
+                scanList = new List<string>();
+                // scanlistに要素を格納する
+                if (checkedListBox_init.CheckedItems.Count != 0)
+                {
+                    for (int x = 0; x < checkedListBox_init.CheckedItems.Count; x++)
+                    {
+                        scanList.Add(checkedListBox_init.CheckedItems[x].ToString());
+                    }
+                }
+            }
+
+
+            // 対象のログが流れた際は、座標を取得する（座標取得はデフォルト設定）
+            foreach (string scanstr in scanList)
+            {
+                if (logInfo.logLine.Contains(scanstr))
+                {
+                    fileFullPath_main_local_scan = textBoxlocalPath_init.Text;
+                    GetMobInfo(scanstr);
+                }
+
+            }
+            if (logInfo.logLine.Contains("座標取得!"))
+            {
+                GetMobInfo("座標取得");
+            }
+
+        }
+        /// <summary>
+        /// 指定したパスにディレクトリが存在しない場合
+        /// すべてのディレクトリとサブディレクトリを作成します
+        /// </summary>
+        public static DirectoryInfo SafeCreateDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return null;
+            }
+            return Directory.CreateDirectory(path);
+        }
+
+        /// <summary>
+        /// Mob情報を取得する
+        /// </summary>
+        private void GetMobInfo(string FileName)
+        {
+
+            dynamic list = ActHelper.GetCombatantList();
+
+            List<CombertBean> CombertBeanList = new List<CombertBean>();
+
+            foreach (dynamic item in list.ToArray())
+            {
+                if (item == null)
+                {
+                    continue;
+                }
+
+                var combatant = new Combatant();
+                combatant.Name = (string)item.Name;
+                combatant.ID = (uint)item.ID;
+                combatant.Job = (int)item.Job;
+                combatant.IsCasting = (bool)item.IsCasting;
+                combatant.OwnerID = (uint)item.OwnerID;
+                combatant.type = (byte)item.type;
+                combatant.Level = (int)item.Level;
+                combatant.CurrentHP = (int)item.CurrentHP;
+                combatant.MaxHP = (int)item.MaxHP;
+                combatant.PosX = (float)item.PosX;
+                combatant.PosY = (float)item.PosY;
+                combatant.PosZ = (float)item.PosZ;
+
+                CombertBean combertBean = new CombertBean();
+                combertBean.Name = combatant.Name.ToString();
+                combertBean.ID = combatant.ID;
+                combertBean.MaxHp = combatant.MaxMP;
+                combertBean.CurrentHP = combatant.CurrentHP;
+                combertBean.Job = combatant.Job;
+                combertBean.IsCasting = combatant.IsCasting;
+                combertBean.OwnerID = combatant.OwnerID;
+                combertBean.type = combatant.type;
+                combertBean.Level = combatant.Level;
+                combertBean.X = combatant.PosX.ToString();
+                combertBean.Y = combatant.PosY.ToString();
+                combertBean.Z = combatant.PosZ.ToString();
+
+                CombertBeanList.Add(combertBean);
+            }
+
+            // ファイルを出力する
+            this.FilePush(CombertBeanList, FileName);
+
+        }
+        /// <summary>
+        /// ログを出力するメソッド
+        /// </summary>
+        /// <param name="MobName"></param>
+        /// <param name="MaxHp"></param>
+        /// <param name="CurrentHP"></param>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        private void FilePush(List<CombertBean> CombertBeanList, string FileName)
+        {
+            string FileOutPath = @fileFullPath_main_local_scan;
+            // ファイル出力先を作成する
+            if (!(FileOutPath.Substring(FileOutPath.Length - 1)).Equals("\\"))
+            {
+                FileOutPath = FileOutPath + "\\";
+            }
+
+
+            // ファイルの存在チェックを実施し、存在しない名前になるまで連番作成を行う
+            int i = 1;
+
+            string Path = FileOutPath + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + FileName;
+            while (File.Exists(Path + i + ".txt"))
+            {
+                i++;
+            }
+            Path = Path + i + ".txt";
+
+            string OutPutString = "";
+            string 改行 = "\r\n";
+
+            foreach (CombertBean combertBean in CombertBeanList)
+            {
+                OutPutString += "ID：" + combertBean.ID + 改行;
+                OutPutString += "名前：" + combertBean.Name + 改行;
+                OutPutString += "X：" + combertBean.X + 改行;
+                OutPutString += "Y：" + combertBean.Y + 改行;
+                OutPutString += "Z：" + combertBean.Z + 改行;
+                OutPutString += "Job：" + Job.Instance.GetJobName(combertBean.Job) + 改行;
+                OutPutString += "MaxHp：" + combertBean.MaxHp + 改行;
+                OutPutString += "CurrentHP：" + combertBean.CurrentHP + 改行;
+                OutPutString += "IsCasting：" + combertBean.IsCasting + 改行;
+                OutPutString += "OwnerID：" + combertBean.OwnerID + 改行;
+                OutPutString += "type：" + combertBean.type + 改行;
+                OutPutString += "Level：" + combertBean.Level + 改行;
+                OutPutString += 改行;
+            }
+
+            // UTF - 8で書き込む
+            //書き込むファイルが既に存在している場合は、上書きする
+            StreamWriter sw = new StreamWriter(
+                @Path,
+                false,
+                Encoding.GetEncoding("UTF-8"));
+            //内容を書き込む
+            sw.Write(OutPutString);
+            //閉じる
+            sw.Close();
+        }
+
+        /// <summary>
+        /// addボタン押下時のイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            // まず記載の文字を格納する
+            string addtext = textBox_list.Text;
+            // 空の場合、処理を中断する
+            if (addtext == "") return;
+            // リストボックスに、アイテムを追加する
+            checkedListBox_init.Items.Add(addtext, true);
+            // テキストボックスの中身を空にする
+            textBox_list.Text = "";
+            scanList = new List<string>();
+            // scanlistに要素を格納する
+            if (checkedListBox_init.CheckedItems.Count != 0)
+            {
+                for (int x = 0; x < checkedListBox_init.CheckedItems.Count; x++)
+                {
+                    scanList.Add(checkedListBox_init.CheckedItems[x].ToString());
+                }
+            }
+        }
+        /// <summary>
+        /// deleteボタン押下時のイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            // アイテムが何も選択されていなかった場合は、処理を中断する
+            if (select_item_name == "") return;
+            // アイテムを削除する
+            checkedListBox_init.Items.Remove(select_item_name);
+        }
+        /// <summary>
+        /// アイテムが選択された時のイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void checkedListBox_init_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // アイテムが0件だった場合、処理を中断する
+            if (checkedListBox_init.Items.Count == 0) return;
+            // アイテム名を格納する
+            select_item_name = (string)checkedListBox_init.SelectedItem;
+
+            scanList = new List<string>();
+            // scanlistに要素を格納する
+            if (checkedListBox_init.CheckedItems.Count != 0)
+            {
+                for (int x = 0; x < checkedListBox_init.CheckedItems.Count; x++)
+                {
+                    scanList.Add(checkedListBox_init.CheckedItems[x].ToString());
+                }
             }
         }
     }
