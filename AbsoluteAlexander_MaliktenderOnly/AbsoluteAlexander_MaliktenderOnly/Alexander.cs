67961ef3db3df2e66097f453c696fb7d21836649
mame <hanaochan@live.jp>
2019-10-27 17:33:19 +0900

    コミット

---
+++ AbsoluteAlexander_MaliktenderOnly/AbsoluteAlexander_MaliktenderOnly/Alexander.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+
+namespace AbsoluteAlexander_MaliktenderOnly
+{
+    public partial class Alexander : UserControl, IActPluginV1
+    {
+        private SettingsSerializer xmlSettings;
+
+        public Alexander()
+        {
+            InitializeComponent();
+        }
+
+        public void DeInitPlugin()
+        {
+            // ACT終了時の処理
+            // throw new NotImplementedException();
+        }
+
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            //lbStatus = pluginStatusText;   // Hand the status label's reference to our local var
+            pluginScreenSpace.Controls.Add(this);   // Add this UserControl to the tab ACT provides
+            this.Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
+                                        // MultiProject.BasePlugin.xmlSettings = new SettingsSerializer(this); // Create a new settings serializer and pass it this instance
+
+            pluginScreenSpace.Text = "Eden";
+            pluginStatusText.Text = "EdenDesignatedPluginStarts";
+
+            // インターフェイス情報を格納する
+            this.xmlSettings = new SettingsSerializer(this);
+
+            // コントロール情報を取得する
+            Control[] ct = ACTInitSetting.GetAllControls(this);
+
+            // 取得したコントロール情報を全て回し、初期表示用の情報を格納する
+            foreach (Control tempct in ct)
+            {
+                if (tempct.Name.IndexOf("_init") > 0)
+                {
+                    // コントロールリストの情報を格納する
+                    this.xmlSettings.AddControlSetting(tempct.Name, tempct);
+                }
+            }
+
+            // 設定ファイルを読み込む
+            ACTInitSetting.LoadSettings(xmlSettings);
+
+            // ログを取得するイベントを生成する
+            ActGlobals.oFormActMain.OnLogLineRead += OFormActMain_OnLogLineRead;
+
+
+
+            throw new NotImplementedException();
+        }
+
+        private void OFormActMain_OnLogLineRead(bool isImport, LogLineEventArgs logInfo)
+        {
+            // 18文字以下のログは読み捨てる
+            // なぜならば、タイムスタンプ＋ログタイプのみのログだから
+            if (logInfo.logLine.Length <= 18)
+            {
+                return;
+            }
+
+            // 戦闘開始のお知らせ
+            if (logInfo.logLine.Contains("戦闘開始！"))
+            {
+            }
+        }
+    }
+}
