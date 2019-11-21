a950e7021b3a3a53ee6fefaf8bd9961ceedcf637
tendo <tendo@A897111>
2019-11-21 09:56:11 +0900

    コミット漏れ対応

---
+++ AbsoluteAlexander_MaliktenderOnly/AbsoluteAlexander_MaliktenderOnly/Terops上下右左.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace AbsoluteAlexander_MaliktenderOnly
+{
+    partial class Terops上下右左
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Terops上下右左));
+            this.pictureBox下 = new System.Windows.Forms.PictureBox();
+            this.pictureBox左 = new System.Windows.Forms.PictureBox();
+            this.pictureBox上 = new System.Windows.Forms.PictureBox();
+            this.pictureBox右 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox下)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox左)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox上)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox右)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox下
+            // 
+            this.pictureBox下.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox下.Image")));
+            this.pictureBox下.Location = new System.Drawing.Point(151, -1);
+            this.pictureBox下.Name = "pictureBox下";
+            this.pictureBox下.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox下.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox下.TabIndex = 5;
+            this.pictureBox下.TabStop = false;
+            // 
+            // pictureBox左
+            // 
+            this.pictureBox左.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox左.Image")));
+            this.pictureBox左.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox左.Name = "pictureBox左";
+            this.pictureBox左.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox左.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox左.TabIndex = 6;
+            this.pictureBox左.TabStop = false;
+            // 
+            // pictureBox上
+            // 
+            this.pictureBox上.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox上.Image")));
+            this.pictureBox上.Location = new System.Drawing.Point(151, -1);
+            this.pictureBox上.Name = "pictureBox上";
+            this.pictureBox上.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox上.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox上.TabIndex = 7;
+            this.pictureBox上.TabStop = false;
+            // 
+            // pictureBox右
+            // 
+            this.pictureBox右.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox右.Image")));
+            this.pictureBox右.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox右.Name = "pictureBox右";
+            this.pictureBox右.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox右.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox右.TabIndex = 8;
+            this.pictureBox右.TabStop = false;
+            // 
+            // Terops上下右左
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 151);
+            this.Controls.Add(this.pictureBox下);
+            this.Controls.Add(this.pictureBox左);
+            this.Controls.Add(this.pictureBox上);
+            this.Controls.Add(this.pictureBox右);
+            this.Name = "Terops上下右左";
+            this.Text = "TeropsABCD";
+            this.Load += new System.EventHandler(this.TeropsABCD_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox下)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox左)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox上)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox右)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox下;
+        public System.Windows.Forms.PictureBox pictureBox左;
+        public System.Windows.Forms.PictureBox pictureBox上;
+        public System.Windows.Forms.PictureBox pictureBox右;
+    }
+}
